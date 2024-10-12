c0cd6abaa06a3a4e8ec43d73e13fc02682288b84
Roman <hilixhilix1@gmail.com>
2024-10-12 15:27:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let result = 5 + 5 + '5';
+console.log(`Результат: ${result}, Тип змінної: ${typeof result}`);
+
+
+let email = "your_email@example.com"; 
+let containsAtSymbol = email.includes('@');
+let totalCharacters = email.length;
+console.log(`Чи містить символ @: ${containsAtSymbol}, Кількість символів: ${totalCharacters}`);
+
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+let fullName = `${word1} ${word2} ${word3} Viktor`;
+console.log(`Повне ім'я: ${fullName}`);
+
+
+let userName = "Олександр";
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`);
